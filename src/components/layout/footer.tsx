09ddb61029e83e6f0a1288394b6dfe3457c21cca
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2024-08-23 22:05:36 -0400

    feat: add contact link

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { VITE_MY_WEBSITE_URL } from '@/config/constants';
 
 export function Footer() {
   return (
     <footer>
-      <p> Made with ❤️ by @gabrieldariodiazhernandez</p>
+      <p>
+        Made with ❤️ by{' '}
+        <a href={VITE_MY_WEBSITE_URL} className="contact-link" target="_blank">
+          @gabrieldariodiazhernandez
+        </a>
+      </p>
     </footer>
   );
 }
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,3 +4,4 @@ export const API_BASE_URL = env.VITE_API_BASE_URL;
 export const API_TOKEN_URL = env.VITE_API_TOKEN_URL;
 export const CLIENT_ID = env.VITE_CLIENT_ID;
 export const CLIENT_SECRET = env.VITE_CLIENT_SECRET;
+export const VITE_MY_WEBSITE_URL = env.VITE_MY_WEBSITE_URL;
